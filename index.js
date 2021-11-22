d462a53d880bbe7a4ae91b8d2560e49c107e3536
Bethforbest <bethforbest@gmail.com>
2021-11-23 01:33:12 +0200

    fixed bugs on min-width

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-$('.partners__slider').slick({
+if(window.matchMedia('(min-width: 1366px)').matches) {
+  $('.partners__slider').slick({
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000
   });
+}
+if(window.matchMedia('(min-width: 768px) and (max-width: 1366px)').matches) {
+  $('.partners__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+}
 
+if(window.matchMedia('(min-width: 375px) and (max-width: 768px)').matches) {
+  $('.partners__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+}
